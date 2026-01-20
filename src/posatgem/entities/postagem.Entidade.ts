2a0b7626900b3b55cbd131b9ab75a404348790bf
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2026-01-20 16:59:16 -0300

    create ralations

--- src/posatgem/entities/postagem.Entidade.ts
+++ src/posatgem/entities/postagem.Entidade.ts
@@ -1,6 +1,7 @@
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToMany, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
 
 @Entity({name: "tb_postagens"})
 export class Postagem{
@@ -23,4 +24,10 @@ export class Postagem{
         onDelete: "CASCADE"
     })
     tema: Tema
+
+    @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
+        onDelete: "CASCADE"
+    })
+    usuario: Usuario
+
 }
