ce6b746c3d49ade244ef48bbe989af98b7e1dbe5
Isaías Roberto <76740216+anidio@users.noreply.github.com>
2026-02-03 01:42:28 -0300

    feat: swagger

--- src/posatgem/controllers/postagem.controller.ts
+++ src/posatgem/controllers/postagem.controller.ts
@@ -2,9 +2,12 @@ import { Controller, Get, Post, Put, Delete, HttpCode, HttpStatus, Param, Body, 
 import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 import { PostagemService } from "../services/postagem.service";
 import { Postagem } from "../entities/postagem.entity";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 
+@ApiTags('Postagem')
 @UseGuards(JwtAuthGuard)
 @Controller("/postagens")
+@ApiBearerAuth()
 export class PostagemController {
   constructor(private readonly postagemService: PostagemService) { }
 
--- src/posatgem/entities/postagem.entity.ts
+++ src/posatgem/entities/postagem.entity.ts
@@ -2,29 +2,36 @@ import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToMany, ManyToOne, PrimaryColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Tema } from "../../tema/entities/tema.entity";
 import { Usuario } from "../../usuario/entities/usuario.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({name: "tb_postagens"})
 export class Postagem{
 
     @PrimaryGeneratedColumn()
+    @ApiProperty()
     id: number
 
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
+    @ApiProperty()
     titulo: string
 
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
+    @ApiProperty()
     texto: string
 
     @UpdateDateColumn()
+    @ApiProperty()
     data: Date
 
+    @ApiProperty()
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
         onDelete: "CASCADE"
     })
     tema: Tema
 
+    @ApiProperty()
     @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
         onDelete: "CASCADE"
     })
